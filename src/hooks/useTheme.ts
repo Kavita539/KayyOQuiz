b7407242e32ea795430cc06f9a3c20642ecda675
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-07-16 18:57:38 +0530

    feat: useTheme Hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,28 @@
-const useTheme = () => {
+import React from "react";
+import {
+    useContext
+} from "react";
+import {
+    ThemeContext
+} from "../context";
 
+const useTheme = () => {
+    const {
+        currentTheme,
+        setCurrentTheme
+    } = useContext(ThemeContext);
+
+    const handleCurrentTheme = () => {
+        setCurrentTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
+        localStorage.setItem(
+            "kayy-o-quiz-theme",
+            JSON.stringify(currentTheme !== "light" ? "light" : "dark")
+        );
+    };
+    return {
+        currentTheme,
+        handleCurrentTheme
+    };
 };
 
 export {
