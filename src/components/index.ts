22b5a70a5a427bab56aed6977e942f9a0aaff726
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-07-16 10:57:43 +0530

    Refactor: exported Components

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,23 @@ export {
     CategoryCard
 }
 from "./category-card/CategoryCard";
+export {
+    Navbar
+}
+from "./navbar/Navbar";
+export {
+    Input
+}
+from "./input/Input";
+export {
+    PasswordInput
+}
+from "./input/PasswordInput";
+export {
+    Question
+}
+from "./question-component/Question";
+export {
+    QuizCard
+}
+from "./quiz-card/QuizCard";
